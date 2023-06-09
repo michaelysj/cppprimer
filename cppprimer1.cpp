bd06756c99be1e4151491d313a197d43817276ad
michaelysj <cnysj@126.com>
2023-06-09 10:20:30 -0500

    update for 1.4.1

--- cppprimer1.cpp
+++ cppprimer1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-/*
- * 简单主函数;
- * 读取两个数，求它们的和
- */
+
 int main()
 {
-    // 提示用户输入两个数
-    std::cout << "Enter two numbers: " << std::endl;
-    int v1 = 0, v2 = 0;           // 保存我们读入的输入数据的变更
-    std::cin >> v1 >> v2;         // 读取输入数据
-    std::cout << "The sum of " << v1 << " and " << v2
-              << " is " << v1 + v2 << std::endl;
+    int sum = 0, val = 1;
+    // 只要val的值小于等于10，while循环就会持续执行
+    while (val <= 10)
+    {
+        sum += val;
+        ++val;
+    }
+    std::cout << "Sum of 1 to 10 inclusive is "
+              << sum << std::endl;
     return 0;
 }
