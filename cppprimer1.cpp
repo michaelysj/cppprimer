86a9a0361394368dfad592ff2810dbe3ce7b306f
michaelysj <cnysj@126.com>
2023-06-09 12:32:12 -0500

    update for 1.5.1

--- cppprimer1.cpp
+++ cppprimer1.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
+#include "Sales_item.h"
 
 int main()
 {
-    int currVal = 0, val = 0;
-    if(std::cin>>currVal)
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    if(item1.isbn() == item2.isbn())
     {
-        int cnt = 1;
-        while (std::cin>>val)
-        {
-            if(val==currVal)
-                ++cnt;
-            else{
-                std::cout << currVal << " occurs " << cnt << " times" << std::endl;
-                currVal = val;
-                cnt = 1;
-            }
-        }
-        std::cout << currVal << " occurs " << cnt << " times" << std::endl;
+        std::cout << item1 + item2 << std::endl;
+        return 0;
     }
-    return 0;
+    else
+    {
+        std::cerr << "Data must refer to same ISBN" << std::endl;
+        return -1;
+    }
 }
