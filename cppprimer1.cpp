492e83b41f6221d7ecabfe274f838d9a6beabdcc
michaelysj <cnysj@126.com>
2023-06-09 10:40:19 -0500

    update for 1.4.3

--- cppprimer1.cpp
+++ cppprimer1.cpp
@@ -2,13 +2,9 @@
 
 int main()
 {
-    int sum = 0, val = 1;
-    // 只要val的值小于等于10，while循环就会持续执行
-    for (int val = 1; val <= 10; ++val)
-    {
+    int sum = 0, val = 0;
+    while(std::cin>>val)
         sum += val;
-    }
-    std::cout << "Sum of 1 to 10 inclusive is "
-              << sum << std::endl;
+    std::cout << "Sum is: " << sum << std::endl;
     return 0;
 }
