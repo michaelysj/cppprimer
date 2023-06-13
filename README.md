26c9a715c5d761b50aa683c5865874c76a807777
michaelysj <cnysj@126.com>
2023-06-13 12:55:57 -0500

    update for 1.6

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # cppprimer
 * 盼姐推荐手撸cppprimer代码
 
+# Makefile使用方式
+> make -e OBJ=cppprimer1
+OBJ=后面是文件名去掉.cpp，执行的时候直接./a.out
+
+## 20230613
+* 撸到1.5
 ## 20230607
 * 初始代码库，添加Makefile
--- cppprimer1.cpp
+++ cppprimer1.cpp
@@ -3,16 +3,26 @@
 
 int main()
 {
-    Sales_item item1, item2;
-    std::cin >> item1 >> item2;
-    if(item1.isbn() == item2.isbn())
+    Sales_item total;
+    if(std::cin >> total)
     {
-        std::cout << item1 + item2 << std::endl;
-        return 0;
+        Sales_item trans;
+        while(std::cin >>  trans)
+        {
+            if(total.isbn() == trans.isbn())
+                total += trans;
+            else
+            {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+        std::cout << total << std::endl;
     }
     else
     {
-        std::cerr << "Data must refer to same ISBN" << std::endl;
+        std::cerr << "No data?!" << std::endl;
         return -1;
     }
+    return 0;
 }
